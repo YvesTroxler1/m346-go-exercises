aae9d88983a83fdb1cecb0acc898bfe672a372be
YvesTroxler1 <153171469+YvesTroxler1@users.noreply.github.com>
2024-11-18 09:21:15 +0100

    excersie 1.1, excersice 1.2

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,20 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	var firstName string = "Yves";
+	var lastName string = "Troxler";
+
+	var dayOfBirth uint = 3;
+	var monthOfBirth uint = 8;
+	var yearOfBirth uint16 = 2008;
+
+	var numberOfSiblings uint = 2;
+	
+	var heightInMeters float32 = 1.78;
+
+	var zodiacSign rune = '\u264C';
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
